1f8bdc440a962fdbb11e3fddddca17d892901db4
Martin P. Hellwig <martin@hellwig.uk>
2023-06-21 16:26:08 +0100

    Incorporate .env into docker build/run

--- __project/makefiles/docker.mk
+++ __project/makefiles/docker.mk
@@ -4,17 +4,27 @@ DC=docker compose -f __project/docker/docker-compose.yml --project-directory ./
 build:
 	$(DC) build
 
+# Same as build but adds --progress-plain to it, handy for troubleshooting.
+build-plain:
+	$(DC) build --progress=plain
+
 # Start the compose defined docker environment(s).
 up:
 	$(DC) up -d --force-recreate
 
+# Stop the compose defined docker environment(s).
 down:
 	$(DC) down
 
+# Create a user shell on work.
 shell:
-	@$(DC) exec work /application/__project/scripts/add_passwd_line.sh
 	@$(DC) exec -it -u $(UID) work /bin/bash
 
+# Create a root shell on work.
 root:
 	@$(DC) exec -it work /bin/bash
+
+# Spin up a base image and create a shell.
+base:
+	@$(DC) run -it --rm base /bin/bash
 
--- __project/scripts/add_passwd_line.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env sh
-
-if grep :$UID:$GID: < /etc/passwd
-then
-   :
-else
-   echo $PWDLINE >> /etc/passwd
-fi
